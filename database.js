3c00fc720e2e04498dbc51b0bd2a44a45cb01a70
tip <lokasun@icloud.com>
2018-12-15 07:50:08 +0700

    ready for heroku

--- database.js
+++ database.js
@@ -1,5 +1,5 @@
 const pgb = require('pg-promise')();
-var db = pgb('ec2-54-243-147-162.compute-1.amazonaws.com?ssl=true');
+var db = pgb('postgres://mqvvplpgwkwaww:bbe283a5d4ce86d696c0794bfca51f66630f8553225b83bff3880b169880250a@ec2-54-243-147-162.compute-1.amazonaws.com:5432/ddhq6akm6qbbtp?ssl=true');
 
 function getAllCustomers(req, res) {
     db.any('select * from customers')
@@ -14,4 +14,11 @@ function getAllCustomers(req, res) {
         .catch(function (error) {
             console.log('ERROR:', error)
         })
+}
+
+module.exports ={
+
+    getAllCustomers
+
+
 }
--- server.js
+++ server.js
@@ -1,34 +1,25 @@
 var express = require('express');
 var app = express();
-var db = require('./database')
-var cors = require('cors')
+var db = require('./database');
 
-app.use(cors())
+var bodyParser = require('body-parser');
 app.use(bodyParser.json()); 
 app.use(bodyParser.urlencoded({
     extended: true
 }));
+
+var cors = require('cors');
+app.use(cors());
 
 
 app.get('/', function (req, res) {
     res.send('Express is running');
 });
 
-
-
-var output = {
-    status: 'success',
-    message: 'REST API is working'
-}
-
-
-app.get('/api/customers', function (req, res) {
-    res.status(500).json(output);
-
-});
-
-
 app.get('/api/customers', db.getAllCustomers);
 
 
-
+var port = process.env.PORT || 8080;
+app.listen(port, function () {
+    console.log('App is running on http://localhost:' + port);
+});
